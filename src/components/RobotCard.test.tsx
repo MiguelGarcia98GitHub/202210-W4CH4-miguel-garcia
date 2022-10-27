8478092253b50a0a9d003b95bcc03252d05da06b
MiguelGarcia98Github <miguelgarciard98@gmail.com>
2022-10-27 23:24:55 +0200

    Added test to RobotCard component

--- src/components/RobotCard.test.tsx
+++ src/components/RobotCard.test.tsx
@@ -1,4 +1,5 @@
 import { screen, render } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
 import { RobotCard } from "./RobotCard";
 
 describe("Given CreateRobot Component", () => {
@@ -30,6 +31,24 @@ describe("Given CreateRobot Component", () => {
             render(<RobotCard data={mockRobotData} />);
             const element = screen.getByText(/Speed/i);
             expect(element).toBeInTheDocument();
+        });
+        test("Then it should have a function called submitForm", () => {
+            const mockRobotData = {
+                id: Math.floor(Math.random() * 1000000),
+                name: "Terminator",
+                imageURL:
+                    "https://static.abc.es/media/tecnologia/2017/09/25/inteligencia-artificial-terminator-kKjH--1240x698.jpg",
+                speed: 8,
+                resistance: 9,
+                creationDate: new Date().toLocaleDateString(),
+            };
+            const mocksubmitForm = jest.fn();
+            render(<RobotCard data={mockRobotData} />);
+            const element = screen.getByText(/Delete/i);
+            element.addEventListener("click", mocksubmitForm);
+            userEvent.click(screen.getByText(/Delete/i));
+
+            expect(mocksubmitForm).toHaveBeenCalledTimes(1);
         });
     });
 });
